06350b46630cbcdbf5cd57b126a32bc7984bf68a
KariHab <habboutk@outlook.com>
2023-06-24 14:42:35 -0400

    done

--- retrieve_jobs.py
+++ retrieve_jobs.py
@@ -1,7 +1,11 @@
+# A simple job aggregator from  Hacker News with the use of it's API
+# Karima H - June 2023
+
 import requests
 import time
 import pyfiglet as pf
 
+#make a call to the API
 print('\033[1;33;40m')
 print('\033[1;33;40m',pf.figlet_format('Hacker News Jobs'),'\033[1;37;40m')
 url = 'https://hacker-news.firebaseio.com/v0/jobstories.json'
@@ -10,6 +14,8 @@ result = requests.get(url)
 offer_ids = result.json()
 offers = []
 print("...Gathering your job offers...")
+
+#process each jobs with their info
 for offer_id in offer_ids[:10]:
     url = f"https://hacker-news.firebaseio.com/v0/item/{offer_id}.json"
     result = requests.get(url)
@@ -21,7 +27,7 @@ for offer_id in offer_ids[:10]:
     }
     offers.append(offer)
 
-
+#print out all the info collected from each job offer
 for offer in offers:
     print(f"\033[1;34;40m\nOffer: {offer['title']}\033[1;37;40m")
     print(f"Author: {offer['author']}")
